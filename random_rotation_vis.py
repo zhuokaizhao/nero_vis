24bf68b08e47faf4e8eea5c6d41a53eec560fde7
Zhuokai Zhao <zhuokai@uchicago.edu>
2021-10-24 00:01:08 -0500

    Testing von mises distribution.

--- random_rotation_vis.py
+++ random_rotation_vis.py
@@ -5,21 +5,34 @@ from matplotlib import pyplot as plt
 
 
 all_percentages = [33, 66, 100]
-bins = list(range(0, 370, 10))
+bins = list(range(-180, 190, 10))
 
 for percentage in all_percentages:
-    sigma = 360*percentage/150/2
-    # distribution that has the support as the rotation limit
-    a = (0 - 0) / sigma
-    b = (360 - 0) / sigma
+    # von mises
+    mu = 0
+    if percentage == 33:
+        kappa = 100/(4*percentage) * np.pi
+    elif percentage == 66:
+        kappa = 100/(4*percentage) * np.pi
+    elif percentage == 100:
+        kappa = 100/(6*percentage) * np.pi
 
-    all_rots = []
+    all_rots = np.random.vonmises(mu, kappa, 10000) / np.pi * 180
 
-    for i in range(10000):
-        # random rotation
-        rot = float(truncnorm.rvs(a, b, scale=sigma, size = 1)[0])
-        all_rots.append(rot)
+    # # truncated gaussian
+    # sigma = 360*percentage/200/2
+    # # distribution that has the support as the rotation limit
+    # a = (-180 - 0) / sigma
+    # b = (180 - 0) / sigma
+
+    # all_rots = []
+
+    # for i in range(10000):
+    #     # random rotation
+    #     rot = float(truncnorm.rvs(a, b, scale=sigma, size = 1)[0])
+    #     all_rots.append(rot)
 
     plt.hist(all_rots, bins=bins, label=f'{percentage} rotation', alpha=0.3)
+
 plt.legend()
 plt.show()
