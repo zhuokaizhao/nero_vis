ace26589c784c641b94c1a12075905bd18879982
Zhuokai Zhao <zhuokai@uchicago.edu>
2021-11-20 12:15:32 -0600

    Some vis test on multivariate sampler.

--- nero_coco/random_rotation_vis.py
+++ nero_coco/random_rotation_vis.py
@@ -4,6 +4,51 @@ from scipy.stats import truncnorm
 from matplotlib import pyplot as plt
 
 
+import truncated_mvn_sampler
+
+d = 2
+mu = np.zeros(d)
+cov = np.array([[100, 0], [0, 100]])
+lb = np.zeros_like(mu) - 20
+ub = np.zeros_like(mu) + 20
+n_samples = 10000
+tmvn = truncated_mvn_sampler.TruncatedMVN(mu, cov, lb, ub)
+x, y = tmvn.sample(n_samples)
+plt.hist2d(x, y, bins=range(-20, 21))
+plt.show()
+
+# multivariate gaussian
+# mean = [0, 0]
+# cov = [[100, 0], [0, 100]]
+# all_x = []
+# all_y = []
+# for i in range(10000):
+#     x, y = np.random.multivariate_normal(mean, cov, 1).T
+#     print(x, y)
+#     exit()
+# plt.hist2d(all_x, all_y, bins=range(-20, 21))
+# plt.show()
+# exit()
+
+
+# with gaussian, percentage is converted to different sigma value
+sigma_x = 10
+sigma_y = 10
+
+# distribution that has the support as the image boundary
+a = (-20 - 0) / sigma_x
+b = (20 - 0) / sigma_y
+
+left_padding = np.array(truncnorm.rvs(a, b, scale=sigma_x, size = 10000)).astype(int)
+top_padding = np.array(truncnorm.rvs(a, b, scale=sigma_y, size = 10000)).astype(int)
+
+plt.hist2d(left_padding, top_padding, bins=range(-20, 21), alpha=0.7)
+plt.show()
+exit()
+
+
+
+
 all_percentages = [33, 66, 100]
 bins = list(range(-180, 190, 10))
 colors = ['green', 'cyan', 'pink']
