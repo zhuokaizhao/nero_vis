8207b465f72b968ca3d09db3b986037fa08c73a2
Zhuokai Zhao <zhuokai@uchicago.edu>
2022-02-11 15:22:28 -0600

    Complted loading, displaying and resizing image.

--- qt_app/nero_app.py
+++ qt_app/nero_app.py
@@ -1,27 +1,10 @@
 import sys
-import random
+import requests
+# import random
 from PySide6 import QtCore, QtWidgets, QtGui
-from PySide6.QtCore import QObject, QRectF, Qt
-from PySide6.QtWidgets import QFileDialog, QWidget, QVBoxLayout, QGraphicsScene, QGraphicsView
-from PySide6.QtGui import QPixmap
-
-class ImageViewer(QWidget):
-    def __init__(self, image_path):
-        super().__init__()
-
-        self.scene = QGraphicsScene()
-        self.view = QGraphicsView(self.scene)
-        layout = QVBoxLayout()
-        layout.addWidget(self.view)
-        self.setLayout(layout)
-
-        self.load_image(image_path)
-
-    def load_image(self, image_path):
-        pixmap = QPixmap(image_path)
-        self.scene.addPixmap(pixmap)
-        self.view.fitInView(QRectF(0, 0, pixmap.width(), pixmap.height()), Qt.KeepAspectRatio)
-        self.scene.update()
+from PySide6.QtGui  import QPixmap
+from PySide6.QtCore import QObject
+from PySide6.QtWidgets import QFileDialog, QWidget, QLabel
 
 
 class UI_MainWindow(QWidget):
@@ -46,13 +29,29 @@ class UI_MainWindow(QWidget):
 
     @QtCore.Slot()
     def select_images(self):
-        file_path, _ = QFileDialog.getOpenFileName(self, QObject.tr('Load Image'))
-        self.image_viewer = ImageViewer(file_path)
-        self.image_viewer.view()
+        self.image_path, _ = QFileDialog.getOpenFileName(self, QObject.tr('Load Image'))
+        self.display_image()
 
     @QtCore.Slot()
     def select_model(self):
-        file_path, _ = QFileDialog.getOpenFileName(self, QObject.tr('Load Model'))
+        self.model_path, _ = QFileDialog.getOpenFileName(self, QObject.tr('Load Model'))
+
+    def display_image(self):
+        # change current window's title to reflect change
+        self.setWindowTitle(f'Loaded Image {self.image_path}')
+
+        # add a new label for loaded image
+        image_label  = QLabel(self)
+        loaded_image = QtGui.QImage(self.image_path)
+
+        # resize the image to a fixed size
+        image_pixmap = QPixmap(loaded_image.scaledToWidth(100))
+        image_label.setPixmap(image_pixmap)
+
+        # add this image to the display window
+        self.layout.addWidget(image_label)
+
+
 
 
 
