Fixed rotation bugs, better architecture.

qt_app/nero_app.py
@@ -1,8 +1,5 @@
 import sys
-import copy
-import math
 import torch
-import requests
 import torchvision
 import numpy as np
 from PIL import Image
@@ -11,60 +8,10 @@ from PySide6.QtGui  import QPixmap, QFont
 from PySide6.QtCore import QObject
 from PySide6.QtWidgets import QFileDialog, QWidget, QLabel, QRadioButton
 
+import nero_transform
+import nero_utilities
+import nero_run_model
 
-# some helper functions
-def qt_image_to_tensor(img, share_memory=False):
-    """
-    Creates a PyTorch Tensor from a QImage via converting to Numpy array first.
-
-    If share_memory is True, the numpy array and the QImage is shared.
-    Be careful: make sure the numpy array is destroyed before the image,
-                otherwise the array will point to unreserved memory!!
-    """
-    assert isinstance(img, QtGui.QImage), "img must be a QtGui.QImage object"
-    assert img.format() == QtGui.QImage.Format.Format_RGB32, "img format must be QImage.Format.Format_RGB32, got: {}".format(img.format())
-
-    img_size = img.size()
-    buffer = img.constBits()
-
-    # Sanity check
-    n_bits_buffer = len(buffer) * 8
-    n_bits_image  = img_size.width() * img_size.height() * img.depth()
-    assert n_bits_buffer == n_bits_image, \
-        "size mismatch: {} != {}".format(n_bits_buffer, n_bits_image)
-
-    assert img.depth() == 32, "unexpected image depth: {}".format(img.depth())
-
-    # Note the different width height parameter order!
-    arr = np.ndarray(shape  = (img_size.height(), img_size.width(), img.depth()//8),
-                     buffer = buffer,
-                     dtype  = np.uint8)
-
-    if share_memory:
-        return torch.from_numpy(arr)
-    else:
-        return torch.from_numpy(copy.deepcopy(arr))
-
-
-def tensor_to_qt_image(img_pt):
-    img_np = img_pt.numpy()
-    img_qt = QtGui.QImage(img_np.data, img_np.shape[1], img_np.shape[0], QtGui.QImage.Format_BGR888)
-
-    return img_qt
-
-
-# rotate mnist image
-def rotate_mnist_image(image, angle, target_img_size):
-    if target_img_size != 29:
-        raise Exception('Warning: MNIST model takes image size 29')
-    img_size = len(image)
-    # transform includes upsample, rotate, downsample and padding (right and bottom) to image_size
-    image = torchvision.transforms.Resize(img_size*3)(image)
-    image = torchvision.transforms.RandomRotation(degrees=(angle, angle), resample=Image.BILINEAR, expand=False)(image)
-    image = torchvision.transforms.Resize(img_size)(image)
-    image = torchvision.transforms.Pad(((target_img_size-img_size)//2, (target_img_size-img_size)//2, (target_img_size-img_size)//2+1, (self.target_img_size-self.img_size)//2+1), fill=0, padding_mode='constant')(image)
-
-    return image
 
 class UI_MainWindow(QWidget):
     def __init__(self):
@@ -87,6 +34,10 @@ class UI_MainWindow(QWidget):
         # future loaded images and model layout
         self.loaded_layout = QtWidgets.QGridLayout()
         self.loaded_layout.setContentsMargins(30, 50, 30, 50)
+        # buttons layout for run model
+        self.run_button_layout = QtWidgets.QGridLayout()
+        # warning text layout
+        self.warning_layout = QtWidgets.QVBoxLayout()
 
         # title of the application
         self.title = QLabel('Non-Equivariance Revealed on Orbits',
@@ -114,13 +65,17 @@ class UI_MainWindow(QWidget):
 
         # load data button
         self.data_button = QtWidgets.QPushButton('Load Test Image')
-        # self.data_button.setFixedSize(QtCore.QSize(300, 100))
         self.load_button_layout.addWidget(self.data_button)
         self.data_button.clicked.connect(self.load_image_clicked)
         # load model button
         self.model_button = QtWidgets.QPushButton('Load Model')
         self.load_button_layout.addWidget(self.model_button)
         self.model_button.clicked.connect(self.load_model_clicked)
+
+        # load data button
+        self.run_once_button = QtWidgets.QPushButton('Run once')
+        self.run_button_layout.addWidget(self.run_once_button)
+        self.run_once_button.clicked.connect(self.run_once_button_clicked)
 
         # add individual layouts to the display general layout
         self.layout.addLayout(self.title_layout)
@@ -140,6 +95,8 @@ class UI_MainWindow(QWidget):
 
         # total rotate angle
         self.total_rotate_angle = 0
+
+
 
     @QtCore.Slot()
     def radio_button_1_clicked(self):
@@ -184,12 +141,25 @@ class UI_MainWindow(QWidget):
         # change the button text
         self.model_button.setText(f'Loaded model {model_name}. Click to load new model')
 
+    @QtCore.Slot()
+    def run_once_button_clicked(self):
+        # check if both input image and model are ready
+        if self.image_existed and self.model_existed:
+            nero_run_model.run_mnist_once()
+        else:
+            # display a warning text
+
+
+    @QtCore.Slot()
+    def run_all_button_clicked()
+
     def display_image(self, image_existed, image_size):
 
         # load the image and scale the size
-        self.loaded_image = QtGui.QImage(self.image_path)
+        # self.loaded_image = QtGui.QImage(self.image_path)
+        self.loaded_image_pt = torch.from_numpy(np.asarray(Image.open(self.image_path)))[:, :, None]
         # save a numpy copy of the loaded image
-        self.loaded_image_pt = qt_image_to_tensor(self.loaded_image, share_memory=False)
+        self.loaded_image = nero_utilities.tensor_to_qt_image(self.loaded_image_pt)
         # resize the display QImage
         self.loaded_image = self.loaded_image.scaledToWidth(image_size)
 
@@ -199,9 +169,9 @@ class UI_MainWindow(QWidget):
         # add a new label for loaded image if no image has existed
         if not image_existed:
             # set minimum size to prepare for later rotation
-            diag = (self.image_pixmap.width()**2 + self.image_pixmap.height()**2)**0.5
+            # diag = (self.image_pixmap.width()**2 + self.image_pixmap.height()**2)**0.5
             self.image_label = QLabel(self)
-            self.image_label.setMinimumSize(diag, diag)
+            # self.image_label.setMinimumSize(diag, diag)
             self.image_label.setAlignment(QtCore.Qt.AlignCenter)
 
         # put pixmap in the label
@@ -254,31 +224,33 @@ class UI_MainWindow(QWidget):
             print('translating')
         # when in rotation mode
         elif self.rotation:
-            self.all_mouse_x.append(event.x())
-            self.all_mouse_y.append(event.y())
+            self.all_mouse_x.append(event.position().x())
+            self.all_mouse_y.append(event.position().y())
             if len(self.all_mouse_x) > 2:
                 self.all_mouse_x.pop(0)
                 self.all_mouse_y.pop(0)
             # angle = math.atan2(event.y() - self.all_mouse_y[-2], event.x() - self.all_mouse_x[-2]) / math.pi * 180
             # naive way to determine rotate angle
-            if self.all_mouse_x[-1] > self.all_mouse_x[0] or self.all_mouse_y[-1] > self.all_mouse_y[0]:
+            if self.all_mouse_x[-1] < self.all_mouse_x[0] or self.all_mouse_y[-1] < self.all_mouse_y[0]:
                 self.total_rotate_angle += 1
             else:
                 self.total_rotate_angle -= 1
 
-            # rotate the image
-            cur_image_pt = rotate_mnist_image(self.loaded_image_pt, self.total_rotate_angle, 29)
+            # rotate the image tensor
+            cur_image_pt = nero_transform.rotate_mnist_image(self.loaded_image_pt, self.total_rotate_angle)
             # self.image_pixmap = self.image_pixmap.transformed(QtGui.QTransform().rotate(angle), QtCore.Qt.SmoothTransformation)
-            # convert image tensor to qt
-            cur_image = tensor_to_qt_image(cur_image_pt)
+            # convert image tensor to qt image
+            self.cur_image = nero_utilities.tensor_to_qt_image(cur_image_pt)
+            # resize the at image
+            self.cur_image = self.cur_image.scaledToWidth(self.image_size)
             # update the pixmap and label
-            self.image_pixmap = QPixmap(cur_image)
+            self.image_pixmap = QPixmap(self.cur_image)
             self.image_label.setPixmap(self.image_pixmap)
 
     def mousePressEvent(self, event):
         print("mousePressEvent")
-        self.all_mouse_x = [event.x()]
-        self.all_mouse_y = [event.y()]
+        self.all_mouse_x = [event.position().x()]
+        self.all_mouse_y = [event.position().y()]
 
     def mouseReleaseEvent(self, event):
         print("mouseReleaseEvent")