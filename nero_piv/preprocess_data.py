d2aa294997df272c275d64bf7d187f74752be5fc
Zhuokai Zhao <zhuokai@uchicago.edu>
2022-05-12 18:10:47 -0500

    Made progress.

--- nero_piv/preprocess_data.py
+++ nero_piv/preprocess_data.py
@@ -1,10 +1,9 @@
 # The file preprocesses Cai's original dataset to add restrictions that demonstrate NERO plots
 import os
-import sys
-import time
-import torch
+import glob
 import argparse
 import numpy as np
+import flowiz as fz
 from PIL import Image
 
 
@@ -33,9 +32,32 @@ def main():
         print(f'Output data dir: {output_dir}')
         print(f'Data restriction direction: {restriction}\n')
 
+    # obtain all the data paths
+    img1_name_list = []
+    img2_name_list = []
+    label_name_list = []
+    for cur_dir in os.listdir(input_dir):
+        img1_name_list.extend(glob.glob(os.path.join(input_dir, cur_dir, '*img1.tif')))
+        img2_name_list.extend(glob.glob(os.path.join(input_dir, cur_dir + '*img2.tif')))
+        label_name_list.extend(glob.glob(os.path.join(input_dir, cur_dir + '/*flow.flo')))
 
+    amount = len(label_name_list)
+    # load the first sample to determine the image size
+    temp = np.asarray(Image.open(img1_name_list[0])) * 1.0 / 255.0
+    img_height, img_width = temp.shape
+
+    # load labels to check for later restrictions
+    labels = np.zeros((amount, img_height, img_width, 2))
+    for i in range(amount):
+        labels[i] = fz.read_flow(label_name_list[i])
+
+
+    # different preprocessing for different restrictions
+    restricted_img1_name_list = []
+    restricted_img2_name_list = []
+    restructed_label_name_list = []
     if restriction == 'right':
-        print('hh')
+
 
 
 
