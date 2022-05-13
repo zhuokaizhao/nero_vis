0f6c6c72024b3e33e813bf02ec8106633ff60b1d
Zhuokai Zhao <zhuokai@uchicago.edu>
2022-05-13 00:12:13 -0500

    Finished preprocess part, updated gitignore.

--- nero_piv/preprocess_data.py
+++ nero_piv/preprocess_data.py
@@ -1,6 +1,7 @@
 # The file preprocesses Cai's original dataset to add restrictions that demonstrate NERO plots
 import os
 import glob
+import shutil
 import argparse
 import numpy as np
 import flowiz as fz
@@ -38,8 +39,18 @@ def main():
     label_name_list = []
     for cur_dir in os.listdir(input_dir):
         img1_name_list.extend(glob.glob(os.path.join(input_dir, cur_dir, '*img1.tif')))
-        img2_name_list.extend(glob.glob(os.path.join(input_dir, cur_dir + '*img2.tif')))
-        label_name_list.extend(glob.glob(os.path.join(input_dir, cur_dir + '/*flow.flo')))
+        img2_name_list.extend(glob.glob(os.path.join(input_dir, cur_dir,'*img2.tif')))
+        label_name_list.extend(glob.glob(os.path.join(input_dir, cur_dir, '*flow.flo')))
+
+    # sort the list so that they match in order
+    img1_name_list.sort()
+    img2_name_list.sort()
+    label_name_list.sort()
+
+    if len(img1_name_list) != len(img2_name_list):
+        raise Exception(f'Number of image 1 names ({len(img1_name_list)}) does not match with the number of image 2 names({len(img2_name_list)})')
+    if len(img2_name_list) != len(label_name_list):
+        raise Exception(f'Number of image names ({len(img1_name_list)}) does not match with the number of labels({len(label_name_list)})')
 
     amount = len(label_name_list)
     # load the first sample to determine the image size
@@ -55,8 +66,65 @@ def main():
     # different preprocessing for different restrictions
     restricted_img1_name_list = []
     restricted_img2_name_list = []
-    restructed_label_name_list = []
-    if restriction == 'right':
+    restricted_label_name_list = []
+
+    for i in range(amount):
+        cur_label = labels[i]
+        qualified = False
+        if restriction == 'left-only':
+            # compute the average of x velocity
+            x_avg = np.mean(cur_label[:, :, 0])
+
+            if x_avg < 0:
+                qualified = True
+
+        elif restriction == 'top-only':
+            # compute the average of y velocity
+            x_avg = np.mean(cur_label[:, :, 1])
+
+            if x_avg > 0:
+                qualified = True
+
+        elif restriction == 'right-only':
+            # compute the average of y velocity
+            x_avg = np.mean(cur_label[:, :, 0])
+
+            if x_avg > 0:
+                qualified = True
+
+        elif restriction == 'down-only':
+            # compute the average of y velocity
+            x_avg = np.mean(cur_label[:, :, 1])
+
+            if x_avg < 0:
+                qualified = True
+
+        if qualified:
+            restricted_img1_name_list.append(img1_name_list[i])
+            restricted_img2_name_list.append(img2_name_list[i])
+            restricted_label_name_list.append(label_name_list[i])
+
+    print(f'{len(restricted_img1_name_list)} ({len(restricted_img1_name_list)/len(img1_name_list)*100:.2f}%) samples qualified\n')
+
+    # copy and paste these qualified data to output dir
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    for i in range(len(restricted_img1_name_list)):
+        cur_img1_name = restricted_img1_name_list[i].split('/')[-1]
+        cur_img2_name = restricted_img2_name_list[i].split('/')[-1]
+        cur_label_name = restricted_label_name_list[i].split('/')[-1]
+        cur_img1_path = os.path.join(output_dir, cur_img1_name)
+        cur_img2_path = os.path.join(output_dir, cur_img2_name)
+        cur_label_path = os.path.join(output_dir, cur_label_name)
+        shutil.copy(restricted_img1_name_list[i], cur_img1_path)
+        shutil.copy(restricted_img2_name_list[i], cur_img2_path)
+        shutil.copy(restricted_label_name_list[i], cur_label_path)
+
+    print(f'All qualified data has been saved to {output_dir}')
+
+
+
 
 
 
