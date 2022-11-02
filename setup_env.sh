88ad03a1261dff7b9c48c002d146e0e73a87f832
Zhuokai Zhao <zhuokai@uchicago.edu>
2022-11-02 16:51:12 -0500

    Environment setup bash script.

---
+++ setup_env.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# check if NVidia GPU is available
+if [[ $(lshw -C display | grep vendor) =~ "NVIDIA Corporation" ]]; then
+    echo "Installing PyTorch with CUDA"
+    conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia -y
+else
+    echo "Installing CPU-only PyTorch"
+    conda install pytorch torchvision torchaudio cpuonly -c pytorch -y
+fi
+
+# other dependencies
+pip install cupy
+pip install flowiz
+pip install PySide6
+pip install pyqtgraph
+pip install opencv-python
+conda install -c conda-forge tqdm -y
+conda install -c conda-forge pillow -y
+conda install -c conda-forge argparse -y
+conda install -c conda-forge scikit-learn -y
+conda install -c conda-forge umap-learn -y
+pip install e2cnn
